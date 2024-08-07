b69c9dc2e1f10926d1e0e15ff0701aa6d37057f3
amitkumar1528 <amitsoftwaree@gmail.com>
2024-08-07 12:04:51 +0530

    change

--- AZURE_gitapp/obj/Debug/net8.0/AZURE_gitapp.AssemblyInfo.cs
+++ AZURE_gitapp/obj/Debug/net8.0/AZURE_gitapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AZURE_gitapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5331bab6882071fda1ce6bf20a5478dec93a60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324fff1720201d5d39c3351b51904713afae4a28")]
 [assembly: System.Reflection.AssemblyProductAttribute("AZURE_gitapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AZURE_gitapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
